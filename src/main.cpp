e8ba0e49f686a16b536d619632969a4bb01205d8
onlydcx <rin.hanaoka@icloud.com>
2023-08-30 21:37:26 +0900

    下半分のライン読めた&表示

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,49 @@
 Motor motor;
 
 int MuxPins[8] = {13,16,20,21,32,33,10,17};
-int LineOUT[2] = {10,11};
+int LineOUT[2] = {11,10};
 
 void setup() {
   Serial.begin(9600);
   for(int i = 0; i < 8; i++) {
-    pinMode(i,OUTPUT);
+    pinMode(MuxPins[i],OUTPUT);
   }
   pinMode(LineOUT[0],INPUT);
   pinMode(LineOUT[1],INPUT);
 }
 
-// D49 [0,0,0,0,0,0,0,0] 0
-
 void loop() {
-  for(int i = 0; i < 8; i++) {
-    digitalWrite(MuxPins[i],LOW);
+  int MuxCode[2][16][4] = {
+    {
+    {0,0,0,0},{1,0,0,0},{0,1,0,0},{1,1,0,0},
+    {0,0,1,0},{1,0,1,0},{0,1,1,0},{1,1,1,0},
+    {0,0,0,1},{1,0,0,1},{0,1,0,1},{1,1,0,1},
+    {0,0,1,1},{1,0,1,1},{0,1,1,1},{1,1,1,1}
+  },
+  {
+    {0,0,0,0},{1,0,0,0},{0,1,0,0},{1,1,0,0},
+    {0,0,1,0},{1,0,1,0},{0,1,1,0},{1,1,1,0},
+    {0,0,0,1},{1,0,0,1},{0,1,0,1},{1,1,0,1},
+    {0,0,1,1},{1,0,1,1},{0,1,1,1},{1,1,1,1}
   }
-  Serial.println(analogRead(LineOUT[0]));
-  // for(int i = 0; i < 4; i++) {
-  //   motor.roll(i,180);
-  // }
+  };
+
+  int cnt = 0;
+  for(int i = 0; i < 2; i++) {
+    for(int j = 0; j < 2; j++) {
+      for(int k = 0; k < 2; k++) {
+        for(int l = 0; l < 2; l++) {
+          int mode = 0;
+          digitalWrite(MuxPins[0],MuxCode[mode][cnt][0]);
+          digitalWrite(MuxPins[1],MuxCode[mode][cnt][1]);
+          digitalWrite(MuxPins[2],MuxCode[mode][cnt][2]);
+          digitalWrite(MuxPins[3],MuxCode[mode][cnt][3]);
+          Serial.print(analogRead(LineOUT[mode]) > 900);
+          Serial.print(" ");
+          cnt++;
+        }
+      }
+    }
+  }
+  Serial.println("");
 }
